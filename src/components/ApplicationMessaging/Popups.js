fce08b752f10f2a66e561c06cf07f45f68de4dcc
Serhii Harmash <garmashs@gmail.com>
2021-10-25 21:55:52 +0300

    - removed react-redux - removed unused dependencies - refactoring - working on MediaGallery implementation - working on EntityEditor implementation - migrated MediaFileViewer and AlarmViewer to EntityEditor - added suspend alarm profile UI

---
+++ src/components/ApplicationMessaging/Popups.js
@@ -0,0 +1,41 @@
+import {showMessage} from 'react-native-flash-message';
+
+export default class Popups {
+  static showError(message) {
+    showMessage({
+      message,
+      type: 'danger',
+      duration: 10000,
+    });
+  }
+
+  static showSuccess(message) {
+    showMessage({
+      message,
+      type: 'success',
+      duration: 2000,
+    });
+  }
+
+  static showWarning(message) {
+    showMessage({
+      message,
+      type: 'warning',
+      duration: 10000,
+    });
+  }
+
+  static showInformation(message) {
+    showMessage({
+      message,
+      type: 'info ',
+      duration: 5000,
+    });
+  }
+}
+
+const showError = Popups.showError;
+const showSuccess = Popups.showSuccess;
+const showWarning = Popups.showWarning;
+const showInformation = Popups.showInformation;
+export {showError, showSuccess, showWarning, showInformation};
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import React from 'react';
+import {StyleSheet} from 'react-native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -7,198 +7,95 @@ import LiveStreaming from './LiveStreaming';
 import Settings from './Settings';
 import AlarmProfileEntityEditor from './Alarm';
 import MyAccount from './MyAccount';
-import messaging from '@react-native-firebase/messaging';
-import AuthService from '../../helpers/AuthService';
-import {Overlay, Divider, Text} from 'react-native-elements';
 import {GlobalStyles} from '../../globals/GlobalStyles';
 import {DefaultNavigationBar} from '../../components';
 import MediaGalleryNavigator from './MediaGallery';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import DefaultSafeAreaView from '../../components/DefaultSafeAreaView';
-import FlashMessage from 'react-native-flash-message';
+import FirebaseMessageHandler from '../../components/ApplicationMessaging/FirebaseMessageHandler';
 
 const Tab = createMaterialTopTabNavigator();
 const iconSize = wp(6);
 
-export default class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      overlay: {
-        visible: false,
-        message: 'test message',
-        title: 'My title',
-      },
-    };
-  }
+const Home = () => {
+  return (
+    <DefaultSafeAreaView>
+      <DefaultNavigationBar />
+      <Tab.Navigator
+        initialRouteName="Live"
+        swipeEnabled={false}
+        tabBarPosition="bottom"
+        scrollEnabled={true}
+        tabBarOptions={{
+          activeTintColor: GlobalStyles.violetColor,
+          inactiveTintColor: GlobalStyles.lightGreyColor,
+          pressColor: GlobalStyles.lightVioletColor,
+          showIcon: true,
+          label: {fontSize: GlobalStyles.defaultFontSize, textTransform: 'none', padding: 0, margin: 0},
+          iconStyle: {
+            height: hp(6),
+            width: hp(6),
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: 0,
+            margin: 0,
+          },
+          tabStyle: {
+            height: hp(12),
+            width: wp(20),
+            alignItems: 'center',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            padding: 0,
+            margin: 0,
+          },
+          style: styles.barStyle,
+        }}>
+        <Tab.Screen
+          name="Live Streaming"
+          component={LiveStreaming}
+          options={{
+            tabBarIcon: ({focused, color}) => <FontAwesome name="video-camera" focused={focused} color={color} size={iconSize} />,
+          }}
+        />
+        <Tab.Screen
+          name="Media Gallery"
+          component={MediaGalleryNavigator}
+          options={{
+            tabBarIcon: ({color}) => <Ionicons name="images" color={color} size={iconSize} />,
+          }}
+        />
+        <Tab.Screen
+          name="Alarm Settings"
+          component={AlarmProfileEntityEditor}
+          options={{
+            tabBarIcon: ({color}) => <FontAwesome name="bell" color={color} size={iconSize} />,
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarIcon: ({color}) => <Ionicons name="ios-settings" color={color} size={iconSize} />,
+          }}
+        />
+        <Tab.Screen
+          name="My Account"
+          component={MyAccount}
+          options={{
+            tabBarIcon: ({color}) => <FontAwesome name="user" color={color} size={iconSize} />,
+          }}
+        />
+      </Tab.Navigator>
+      <FirebaseMessageHandler />
+    </DefaultSafeAreaView>
+  );
+};
 
-  firebaseInit() {
-    messaging()
-      .getToken()
-      .then((clientToken) => AuthService.updateFirebaseClientToken(clientToken))
-      .catch((error) => {
-        console.log(error);
-      });
-
-    try {
-      messaging().onTokenRefresh((clientToken) => AuthService.updateFirebaseClientToken(clientToken));
-    } catch (e) {
-      console.log('Unable to update client token.');
-    }
-
-    this.firebaseMessageListener = messaging().onMessage((remoteMessage) => {
-      if (remoteMessage.notification) {
-        this.setOverlayMessage(remoteMessage);
-        console.log('Message handled in the foreground!', remoteMessage.notification);
-      }
-    });
-  }
-
-  toggleOverlay = () => {
-    this.setState({
-      overlay: {...this.state.overlay, visible: !this.state.overlay.visible},
-    });
-  };
-
-  setOverlayMessage = (remoteMessage) => {
-    this.setState({
-      overlay: {
-        ...this.state.overlay,
-        visible: true,
-        message: remoteMessage.notification.body,
-        title: remoteMessage.notification.title,
-      },
-    });
-  };
-
-  componentWillUnmount() {
-    this.firebaseMessageListener();
-  }
-
-  componentDidMount() {
-    this.firebaseInit();
-    // Check whether an application is opened by notification from quite state
-    messaging().onNotificationOpenedApp((remoteMessage) => {
-      console.log('Notification caused app to open from background state:', remoteMessage.notification);
-      this.setOverlayMessage(remoteMessage);
-    });
-    // Check whether an initial notification is available
-    messaging()
-      .getInitialNotification()
-      .then((remoteMessage) => {
-        if (remoteMessage) {
-          console.log('Notification caused app to open from quit state:', remoteMessage.notification);
-          this.setOverlayMessage(remoteMessage);
-        }
-      });
-  }
-
-  render() {
-    return (
-      <DefaultSafeAreaView>
-        <DefaultNavigationBar />
-        <Tab.Navigator
-          initialRouteName="Live"
-          swipeEnabled={false}
-          tabBarPosition="bottom"
-          scrollEnabled={true}
-          tabBarOptions={{
-            activeTintColor: GlobalStyles.violetColor,
-            inactiveTintColor: GlobalStyles.lightGreyColor,
-            pressColor: GlobalStyles.lightVioletColor,
-            showIcon: true,
-            label: {fontSize: GlobalStyles.defaultFontSize, textTransform: 'none', padding: 0, margin: 0},
-            iconStyle: {
-              height: hp(6),
-              width: hp(6),
-              alignItems: 'center',
-              justifyContent: 'center',
-              padding: 0,
-              margin: 0,
-            },
-            tabStyle: {
-              height: hp(12),
-              width: wp(20),
-              alignItems: 'center',
-              flexDirection: 'column',
-              justifyContent: 'flex-start',
-              padding: 0,
-              margin: 0,
-            },
-            style: styles.barStyle,
-          }}>
-          <Tab.Screen
-            name="Live Streaming"
-            component={LiveStreaming}
-            options={{
-              tabBarIcon: ({focused, color}) => <FontAwesome name="video-camera" focused={focused} color={color} size={iconSize} />,
-            }}
-          />
-          <Tab.Screen
-            name="Media Gallery"
-            component={MediaGalleryNavigator}
-            options={{
-              tabBarIcon: ({color}) => <Ionicons name="images" color={color} size={iconSize} />,
-            }}
-          />
-          <Tab.Screen
-            name="Alarm Settings"
-            component={AlarmProfileEntityEditor}
-            options={{
-              tabBarIcon: ({color}) => <FontAwesome name="bell" color={color} size={iconSize} />,
-            }}
-          />
-          {/*<Tab.Screen*/}
-          {/*  name="Devices"*/}
-          {/*  component={Devices}*/}
-          {/*  options={{*/}
-          {/*    tabBarIcon: ({color}) => <MaterialCommunityIcons name="devices" color={color} size={iconSize} />,*/}
-          {/*  }}*/}
-          {/*/>*/}
-          <Tab.Screen
-            name="Settings"
-            component={Settings}
-            options={{
-              tabBarIcon: ({color}) => <Ionicons name="ios-settings" color={color} size={iconSize} />,
-            }}
-          />
-          <Tab.Screen
-            name="My Account"
-            component={MyAccount}
-            options={{
-              tabBarIcon: ({color}) => <FontAwesome name="user" color={color} size={iconSize} />,
-            }}
-          />
-        </Tab.Navigator>
-        <View>
-          <Overlay overlayStyle={styles.overlay} isVisible={this.state.overlay.visible} onBackdropPress={this.toggleOverlay}>
-            <View>
-              <Text h4 style={styles.overlayHeader}>
-                {this.state.overlay.title}
-              </Text>
-              <Divider />
-              <Text>{this.state.overlay.message}</Text>
-            </View>
-          </Overlay>
-        </View>
-        <FlashMessage position="bottom" />
-      </DefaultSafeAreaView>
-    );
-  }
-}
+export default Home;
 
 const styles = StyleSheet.create({
   barStyle: {
     backgroundColor: GlobalStyles.lightBackgroundColor,
-  },
-  overlay: {
-    alignItems: 'stretch',
-    flexDirection: 'column',
-    height: '60%',
-    justifyContent: 'flex-start',
-    width: '90%',
-  },
-  overlayHeader: {
-    textAlign: 'center',
   },
 });
