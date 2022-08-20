d6a769e7de32c629e0d98c6c15d289e311f4d618
enhancer13 <44034271+enhancer13@users.noreply.github.com>
2022-08-20 14:15:26 +0300

    - updated react native to 0.6.94 version

---
+++ ios/OrionMobileClient/AppDelegate.h
@@ -0,0 +1,9 @@
+#import <React/RCTBridgeDelegate.h>
+#import <React/RCTAppSetupUtils.h>
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
